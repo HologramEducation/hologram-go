374ed5c2c4f77a55e8670f27abd4353eb0da8e53
Zheng Hao Tan <tanzhao@umich.edu>
2016-06-17 10:59:42 -0500

    added plan_test

--- Plan_test.go
+++ Plan_test.go
@@ -1,18 +1,31 @@
 package HologramGo
 
 import (
-	"../Hologram-Go"
 	"testing"
+	"fmt"
+	"../Hologram-Go"
+	"encoding/json"
+	"io/ioutil"
 )
 
 func TestGetDeviceDataPlan(t *testing.T) {
-	expectedDeviceName := "SIM Card"
 
-	var plan = GetDeviceDataPlan(1)
+	file, e := ioutil.ReadFile("json/plan.json")
+	if e != nil {
+		fmt.Printf("Error opening file: %v\n", e)
+	}
 
-	deviceName := plan.GetDeviceName()
+	var payload = HologramGo.Placeholder{}
+	err := json.Unmarshal(file, &payload)
+	if err != nil {
+		fmt.Println("Unable to parse file")
+	}
+	var plan = (HologramGo.Plan)(payload["data"].(map[string]interface{}))
 
-	if expectedDeviceName != deviceName {
-		t.Fatalf("Expected %s, got %s", expectedDeviceName, deviceName)
+	// Test for data plan id
+	expectedFloat := (float64)(51)
+	returnedFloat := plan.GetDataPlanId()
+	if expectedFloat != returnedFloat {
+		t.Fatalf("Expected %s, got %s", expectedFloat, returnedFloat)
 	}
 }
