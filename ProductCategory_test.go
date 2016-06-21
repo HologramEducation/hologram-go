4ff4f7f8e2a436c3b4a755b6051215998300db8f
tzhenghao <tanzhao@umich.edu>
2016-06-20 23:11:42 -0500

    moar tests

---
+++ ProductCategory_test.go
@@ -0,0 +1,13 @@
+package HologramGo
+
+import (
+	"testing"
+	"fmt"
+	"../Hologram-Go"
+	"encoding/json"
+	"io/ioutil"
+)
+
+func TestGetProductCategories(t *testing.T) {
+
+}
--- Product_test.go
+++ Product_test.go
@@ -1,27 +1,90 @@
 package HologramGo
 
 import (
-	"../HologramGo"
 	"testing"
+	"fmt"
+	"../Hologram-Go"
+	"encoding/json"
+	"io/ioutil"
 )
 
+// func TestGetProducts(t *testing.T) {
+
+// 	file, e := ioutil.ReadFile("json/device.json")
+// 	if e != nil {
+// 		fmt.Printf("Error opening file: %v\n", e)
+// 	}
+
+// 	var payload = HologramGo.Placeholder{}
+// 	err := json.Unmarshal(file, &payload)
+// 	if err != nil {
+// 		fmt.Println("Unable to parse file")
+// 	}
+// 	var device = (HologramGo.Device)(payload["data"].(map[string]interface{}))
+
+// 	// Check for expected device id
+// 	expectedFloat := (float64)(1)
+// 	returnedFloat := device.GetDeviceId()
+// 	if expectedFloat != returnedFloat {
+// 		t.Fatalf("Expected %s, got %s", expectedFloat, returnedFloat)
+// 	}
+
+// }
+
 func TestGetProduct(t *testing.T) {
-	expectedProductName := "SIM Card"
 
-	var product = GetProduct(1)
-
-	productName := product.GetProductName()
-
-	if expectedProductName != productName {
-		t.Fatalf("Expected %s, got %s", expectedProductName, product.GetProductName())
+	file, e := ioutil.ReadFile("json/product.json")
+	if e != nil {
+		fmt.Printf("Error opening file: %v\n", e)
 	}
-}
 
-// TODO: Add product options and categories.
-func TestGetProductOptions(t *testing.T) {
+	var payload = HologramGo.Placeholder{}
+	err := json.Unmarshal(file, &payload)
+	if err != nil {
+		fmt.Println("Unable to parse file")
+	}
+	var product = (HologramGo.Product)(payload["data"].(map[string]interface{}))
 
-}
+	// Check for expected product id
+	expectedFloat := (float64)(1)
+	returnedFloat := product.GetProductId()
+	if expectedFloat != returnedFloat {
+		t.Fatalf("Expected %s, got %s", expectedFloat, returnedFloat)
+	}
 
-func TestGetProductCategories(t *testing.T) {
+	expectedString := "SIM"
+	returnedString := product.GetProductSku()
+	if expectedString != returnedString {
+		t.Fatalf("Expected %s, got %s", expectedString, returnedString)
+	}
 
+	expectedString = "SIM Card"
+	returnedString = product.GetProductName()
+	if expectedString != returnedString {
+		t.Fatalf("Expected %s, got %s", expectedString, returnedString)
+	}
+
+	expectedString = "The Hologram Global SIM Card. Comes in a variety of sizes for any application. (Pick your data plan when you go to activate the SIM.)"
+	returnedString = product.GetProductDescription()
+	if expectedString != returnedString {
+		t.Fatalf("Expected %s, got %s", expectedString, returnedString)
+	}
+
+	expectedString = "5.00"
+	returnedString = product.GetProductPrice()
+	if expectedString != returnedString {
+		t.Fatalf("Expected %s, got %s", expectedString, returnedString)
+	}
+
+	expectedString = "https://dashboard.hologram.io/img/sim_card.jpg"
+	returnedString = product.GetProductImageUrl()
+	if expectedString != returnedString {
+		t.Fatalf("Expected %s, got %s", expectedString, returnedString)
+	}
+
+	expectedString = "Global SIM Card"
+	returnedString = product.GetProductInvoiceDescription()
+	if expectedString != returnedString {
+		t.Fatalf("Expected %s, got %s", expectedString, returnedString)
+	}
 }
