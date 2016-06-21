0b6e01745ea246a39f2d2378c8a567c0b1749b43
tzhenghao <tanzhao@umich.edu>
2016-06-20 21:58:02 -0500

    added more test cases

--- Plan_test.go
+++ Plan_test.go
@@ -20,11 +20,54 @@ func TestGetDeviceDataPlan(t *testing.T) {
 	if err != nil {
 		fmt.Println("Unable to parse file")
 	}
-	var plan = (HologramGo.Plan)(payload["data"].(map[string]interface{}))
+	var plans = (payload["data"].([]interface{}))
+	var plan = (HologramGo.Plan)(plans[0].(map[string]interface{}))
 
 	// Test for data plan id
 	expectedFloat := (float64)(51)
 	returnedFloat := plan.GetDataPlanId()
+	if expectedFloat != returnedFloat {
+		t.Fatalf("Expected %s, got %s", expectedFloat, returnedFloat)
+	}
+
+	// Test for data plan partner id.
+	expectedFloat = (float64)(1)
+	returnedFloat = plan.GetDataPlanPartnerId()
+	if expectedFloat != returnedFloat {
+		t.Fatalf("Expected %s, got %s", expectedFloat, returnedFloat)
+	}
+
+	// Test for data plan name.
+	expectedString := "1 MB"
+	returnedString := plan.GetDataPlanName()
+	if expectedString != returnedString {
+		t.Fatalf("Expected %s, got %s", expectedString, returnedString)
+	}
+
+	// Test for data plan subscription
+	expectedString = ""
+	returnedString = plan.GetDataPlanDescription()
+	if expectedString != returnedString {
+		t.Fatalf("Expected %s, got %s", expectedString, returnedString)
+	}
+
+	// Test for data plan size
+	expectedFloat = (float64)(1000000)
+	returnedFloat = plan.GetDataPlanSize()
+	if expectedFloat != returnedFloat {
+		t.Fatalf("Expected %s, got %s", expectedFloat, returnedFloat)
+	}
+
+	// Test for data plan billing period
+	expectedFloat = (float64)(24)
+	returnedFloat = plan.GetDataPlanBillingPeriod()
+	if expectedFloat != returnedFloat {
+		t.Fatalf("Expected %s, got %s", expectedFloat, returnedFloat)
+	}
+
+	// Test for data plan carrier id.
+	expectedFloat = (float64)(1)
+	returnedFloat = plan.GetDataPlanCarrierId()
 	if expectedFloat != returnedFloat {
 		t.Fatalf("Expected %s, got %s", expectedFloat, returnedFloat)
 	}
