f91ce569b859ac89dd1f371f8343c3849c5b2327
Zheng Hao Tan <tanzhao@umich.edu>
2016-07-01 14:03:20 -0500

    golint plan.go

--- Plan.go
+++ Plan.go
@@ -6,13 +6,13 @@ import (
 	"strconv"
 )
 
+// Plan is basically the returned Plan type in the response.
 type Plan map[string]interface{}
 
 // Plans is just a list of Plan(s).
 type Plans []interface{}
 
-// TODO: Add Plans data type for this call here.
-// EFFECTS: Returns device data plans.
+// GetDeviceDataPlans returns device data plans.
 func GetDeviceDataPlans() Plans {
 
 	req := createGetRequest("/plans/")
@@ -26,8 +26,7 @@ func GetDeviceDataPlans() Plans {
 	return unmarshallIntoArrayObject(resp)
 }
 
-// REQUIRES: A plan id.
-// EFFECTS: Returns a given device data plan
+// GetDeviceDataPlan returns a given device data plan.
 func GetDeviceDataPlan(planid int) Plan {
 
 	req := createGetRequest("/plans/" + strconv.Itoa(planid))
@@ -47,62 +46,62 @@ func GetDeviceDataPlan(planid int) Plan {
 // GENERIC PLAN GETTER FUNCTIONS
 ///////////////////////////////////////////////////
 
-// EFFECTS: Returns the data plan id.
+// GetDataPlanId returns the data plan id.
 func (plan Plan) GetDataPlanId() float64 {
 	return plan["id"].(float64)
 }
 
-// EFFECTS: Returns the data plan partner id.
+// GetDataPlanPartnerId returns the data plan partner id.
 func (plan Plan) GetDataPlanPartnerId() float64 {
 	return plan["partnerid"].(float64)
 }
 
-// EFFECTS: Returns the data plan name.
+// GetDataPlanName returns the data plan name.
 func (plan Plan) GetDataPlanName() string {
 	return plan["name"].(string)
 }
 
-// EFFECTS: Returns the data plan description.
+// GetDataPlanDescription returns the data plan description.
 func (plan Plan) GetDataPlanDescription() string {
 	return plan["description"].(string)
 }
 
-// EFFECTS: Returns the data size.
+// GetDataPlanSize returns the data size.
 func (plan Plan) GetDataPlanSize() float64 {
 	return plan["data"].(float64)
 }
 
-// EFFECTS: Returns true if it is recurring.
+// IsDataPlanRecurring returns true if it is recurring.
 func (plan Plan) IsDataPlanRecurring() bool {
 	return plan["recurring"].(bool)
 }
 
-// EFFECTS: Returns true if the data plan is enabled.
+// IsDataPlanEnabled returns true if the data plan is enabled.
 func (plan Plan) IsDataPlanEnabled() bool {
 	return plan["enabled"].(bool)
 }
 
-// EFFECTS: Returns the billing period.
+// GetDataPlanBillingPeriod returns the billing period.
 func (plan Plan) GetDataPlanBillingPeriod() float64 {
 	return plan["billingperiod"].(float64)
 }
 
-// EFFECTS: Returns the number of trial days left.
+// GetDataPlanTrialDays returns the number of trial days left.
 func (plan Plan) GetDataPlanTrialDays() float64 {
 	return plan["trialdays"].(float64)
 }
 
-// EFFECTS: Returns the data plan template id.
+// GetDataPlanTemplateId returns the data plan template id.
 func (plan Plan) GetDataPlanTemplateId() float64 {
 	return plan["templateid"].(float64)
 }
 
-// EFFECTS: Returns the carrier id of the data plan.
+// GetDataPlanCarrierId returns the carrier id of the data plan.
 func (plan Plan) GetDataPlanCarrierId() float64 {
 	return plan["carrierid"].(float64)
 }
 
-// EFFECTS: Returns the groupid of the data plan.
+// GetDataPlanGroupId returns the groupid of the data plan.
 func (plan Plan) GetDataPlanGroupId() float64 {
 	return plan["groupid"].(float64)
 }
