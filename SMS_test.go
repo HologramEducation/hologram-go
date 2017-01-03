084a64565881a96e422629e01725fcc4ca0d3c65
Zheng Hao Tan <tanzhao@umich.edu>
2017-01-03 14:45:11 -0600

    added SMS test

--- SMS_test.go
+++ SMS_test.go
@@ -3,13 +3,13 @@ package HologramGo
 import "testing"
 
 func TestGetProductSMS(t *testing.T) {
-	expectedProductName := "SIM Card"
+	//expectedProductName := "SIM Card"
 
-	var product = GetProduct(1)
+	//var product = GetProduct(1)
 
-	productName := product.GetProductName()
+	//productName := product.GetProductName()
 
-	if expectedProductName != productName {
-		t.Fatalf("Expected %s, got %s", expectedProductName, product.GetProductName())
-	}
+	//if expectedProductName != productName {
+		//t.Fatalf("Expected %s, got %s", expectedProductName, product.GetProductName())
+	//}
 }
